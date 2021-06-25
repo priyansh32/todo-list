9afd803a7b1891f8e9d391d8f04b68c72784ff66
Priyansh Patidar <priyanshpatidar.201cs144@nitk.edu.in>
2021-06-25 05:57:15 +0530

    remove url log

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,6 @@ router.put('/done/:id', async (req, res) => {
 //comment this section if you don't have a api token
 router.get('/getQuote', async (req, res) => {
     let url = `https://www.stands4.com/services/v2/quotes.php?uid=${process.env.STANDS4API_uid}&tokenid=${process.env.STANDS4API_tokenid}&searchtype=RANDOM&format=json`
-    console.log(url)
     let quote = await fetch(url)
     quote = await quote.json()
     res.send(quote)
